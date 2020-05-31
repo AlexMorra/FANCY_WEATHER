5768911d09b1d92aef9c7c2bf22702206aa8f1cd
Alexandr Kartynnik <k77.wolf@gmail.com>
2020-05-31 23:11:48 +0300

    v2

--- assets/js/get_weather.js
+++ assets/js/get_weather.js
@@ -1,5 +1,5 @@
 export default function get_weather(location, lang) {
-    return fetch(`http://api.weatherapi.com/v1/forecast.json?key=e6737aa0836d4d5e83d192721201305&q=${location}&days=3&lang=${lang}`)
+    return fetch(`https://api.weatherapi.com/v1/forecast.json?key=e6737aa0836d4d5e83d192721201305&q=${location}&days=3&lang=${lang}`)
         .then(response => {
             if (response.ok) {
                 return response.json()
