c0ea4ea4e39cc1b09c8e81e614a058bc32835402
Alexandr Kartynnik <k77.wolf@gmail.com>
2020-06-01 01:05:28 +0300

    v3

--- assets/js/speech_synthesis.js
+++ assets/js/speech_synthesis.js
@@ -2,15 +2,17 @@ let speech_btn = document.querySelector('.fa-volume-up');
 speech_btn.addEventListener('click', speech_handler);
 
 let volue_el = document.querySelector('.volume');
-let volume = 1;
+let volume = 0.5;
 volue_el.textContent = `${volume * 100}%.`;
 
 // INIT SpeechRecognition
 window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
-let control_recognition = new window.SpeechRecognition();
+export let control_recognition = new window.SpeechRecognition();
 control_recognition.lang = 'ru-RU';
 control_recognition.continuous = true;
 control_recognition.start();
+
+const synth = window.speechSynthesis;
 
 function speech_handler() {
     speech_btn.classList.add('microphone-active');
@@ -23,12 +25,11 @@ function speech_handler() {
     let degrees = document.querySelector('.weather-today-temp').textContent;
     let details = [...document.querySelector('.weather-details').children];
     let details_text = details.reduce((acc, el) => acc += el.textContent + '. ', '');
-    let synth = window.speechSynthesis;
     let message = new SpeechSynthesisUtterance(`${city}${country}. ${date}, ${time}. ${degrees}°. ${details_text}`);
     message.lang = language;
     message.volume = volume;
     language === 'ru' ? message.rate = 1 : message.rate = 0.8;
-    synth.speak(message);
+    setTimeout(() => synth.speak(message), 250);
     message.addEventListener('end', () => {
         window.speechSynthesis.cancel();
         speech_btn.classList.remove('microphone-active');
@@ -48,13 +49,13 @@ function voice_control() {
                 speech_btn.style.transition = '0.5s all';
                 speech_btn.style.color = 'yellow';
                 setTimeout(() => speech_btn.style.color = 'white', 500);
-                volume = volume - 0.1 < 0 ? 0 : volume - 0.1;
+                volume = +(volume - 0.1 < 0 ? 0 : volume - 0.1).toFixed(1);
             }
             if (transcript.toLowerCase().includes('сделай громче')) {
                 speech_btn.style.transition = '0.5s all';
                 speech_btn.style.color = 'green';
                 setTimeout(() => speech_btn.style.color = 'white', 500);
-                volume = volume + 0.1 > 1 ? 1 : volume + 0.1;
+                volume = +(volume + 0.1 > 1 ? 1 : volume + 0.1).toFixed(1);
             }
             setTimeout(() => speech_btn.removeAttribute('style'), 1000);
             volue_el.textContent = `${volume * 100}%.`
